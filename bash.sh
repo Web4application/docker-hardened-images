8be75152887c7a998909c800c94526ab0adb4067
Seriki yakub <sandraisom00@gmail.com>
2025-12-12 07:36:52 +0100

    Update and rename bash to bash.sh

--- bash.sh
+++ bash.sh
@@ -4,3 +4,8 @@ docker run -d --name kserve-localmodelnode-agent \
 docker run -d --name kserve-localmodelnode-agent \
   -p 8080:8080 -p 8081:8081 -p 9443:9443 \
   <your-namespace>/dhi-kserve-localmodelnode-agent:<tag>
+
+git clone https://github.com/AtomicJar/testcontainers-cloud-go-example
+cd testcontainers-cloud-go-example
+go mod download
+go test -v -count=1
